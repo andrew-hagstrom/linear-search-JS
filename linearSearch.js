e4c487374ad9c9079c30e142b03eec5af0970454
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2023-10-09 16:16:21 -0500

    my first commit

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,30 @@
 function linearSearch(searchTerm, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === searchTerm) {
+      console.log(i)
+      return i
+    }
+  }
+  console.log(undefined);
   return undefined;
 }
 
+linearSearch(4, [1, 2, 3]);
+
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  const allIndices = [];
+  for (let i = 0; i < arr.length; i++)
+  {
+    if (arr[i] === searchTerm) {
+      allIndices.push(i);
+    }
+  }
+  if(!allIndices) {
+    return undefined;
+  }
+  console.log(allIndices);
+  return allIndices;
 }
+globalLinearSearch(1, [1, 2, 19, 3, 9, 3, 1, 1]);
 
 module.exports = { linearSearch, globalLinearSearch };
